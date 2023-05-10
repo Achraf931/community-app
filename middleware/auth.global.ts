ee092499324267cda74f7c576409c94ddc430e5c
Charfeddine Hamrouni <chamrouni@mac-chamrouni.local>
2023-05-10 18:47:39 +0200

    fix: add console log

--- middleware/auth.global.ts
+++ middleware/auth.global.ts
@@ -1,6 +1,7 @@
 export default defineNuxtRouteMiddleware((to) => {
     const user = useSupabaseUser()
     // @ts-ignore
+    console.log(user.value)
     if (!user.value && to.path !== '/login' && to.path !== '/register') {
         return navigateTo('/login',{ redirectCode: 301 })
     } else if (user.value && (to.path === '/login' || to.path === '/register')) {
