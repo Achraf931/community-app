c750a915e2c37abc263e286c28113ccc15d713a7
Charfeddine Hamrouni <chamrouni@mac-chamrouni.local>
2023-05-18 14:21:56 +0200

    fix: fix bg color

--- components/ui/answer.vue
+++ components/ui/answer.vue
@@ -21,7 +21,7 @@ const handleFullscreen = () => {
   }
 </script>
 <template>
-  <article ref="answer" :class="isFullscreen ? 'absolute top-0 left-0 w-screen h-screen z-30 items-start p-5 rounded-none bg-custom-light-gray' : 'rounded-3xl relative bg-white'" class="snap-start flex justify-start gap-4 shadow-custom-light-gray shadow-sm p-4">
+  <article ref="answer" :class="isFullscreen ? 'absolute top-0 left-0 w-screen h-screen z-30 items-start p-5 rounded-none bg-white' : 'rounded-3xl relative'" class="snap-start bg-custom-light-gray flex justify-start gap-4 shadow-custom-light-gray shadow-sm p-4">
       <div class="flex flex-col gap-2 justify-between items-center">
         <div>
             <NuxtLink class="contents min-w-[2.5rem]" :to="{ name: 'profile-id', params: { id: props.answer.attributes.author.data.id } }">
@@ -85,10 +85,10 @@ const handleFullscreen = () => {
         <p v-else-if="props.answer.attributes.content.length < 80" :class="more ? 'line-clamp-none' : 'line-clamp-2 text-ellipsis'" class="content text-sm font-medium">{{ props.answer.attributes.content }}</p>
         <p v-if="props.answer.attributes.content.length >= 80 && props.answer.attributes.content.length < 200" class="text-xs font-semibold block text-custom-purple mt-1" @click="more = !more">Lire {{ more ? 'moins' : 'tout' }}</p>
           <template v-else-if="props.answer.attributes.content.length >= 200">
-              <svg v-if="!isFullscreen" @click="handleFullscreen" class="w-4 h-4 ml-auto mt-2 fill-light-gray stroke-light-gray" stroke-width=".2" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor">
+              <svg v-if="!isFullscreen" @click="handleFullscreen" class="w-4 h-4 ml-auto mt2 fill-light-gray stroke-light-gray" stroke-width=".2" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor">
                   <path fill-rule="evenodd" d="M15 3.75a.75.75 0 01.75-.75h4.5a.75.75 0 01.75.75v4.5a.75.75 0 01-1.5 0V5.56l-3.97 3.97a.75.75 0 11-1.06-1.06l3.97-3.97h-2.69a.75.75 0 01-.75-.75zm-12 0A.75.75 0 013.75 3h4.5a.75.75 0 010 1.5H5.56l3.97 3.97a.75.75 0 01-1.06 1.06L4.5 5.56v2.69a.75.75 0 01-1.5 0v-4.5zm11.47 11.78a.75.75 0 111.06-1.06l3.97 3.97v-2.69a.75.75 0 011.5 0v4.5a.75.75 0 01-.75.75h-4.5a.75.75 0 010-1.5h2.69l-3.97-3.97zm-4.94-1.06a.75.75 0 010 1.06L5.56 19.5h2.69a.75.75 0 010 1.5h-4.5a.75.75 0 01-.75-.75v-4.5a.75.75 0 011.5 0v2.69l3.97-3.97a.75.75 0 011.06 0z" clip-rule="evenodd" />
               </svg>
-              <div v-else @click="handleFullscreen" :class="{ 'mt-auto ml-auto mb-10': isFullscreen }" class="flex items-center justify-center p-2.5 bg-white shadow-custom-light-gray shadow-sm rounded-full">
+              <div v-else @click="handleFullscreen" :class="{ 'mt-auto ml-auto mb-10': isFullscreen }" class="flex items-center justify-center p-2.5 bg-custom-light-gray shadow-custom-light-gray shadow-sm rounded-full">
                   <svg class="w-6 h-6 fill-light-gray" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor">
                       <path fill-rule="evenodd" d="M3.22 3.22a.75.75 0 011.06 0l3.97 3.97V4.5a.75.75 0 011.5 0V9a.75.75 0 01-.75.75H4.5a.75.75 0 010-1.5h2.69L3.22 4.28a.75.75 0 010-1.06zm17.56 0a.75.75 0 010 1.06l-3.97 3.97h2.69a.75.75 0 010 1.5H15a.75.75 0 01-.75-.75V4.5a.75.75 0 011.5 0v2.69l3.97-3.97a.75.75 0 011.06 0zM3.75 15a.75.75 0 01.75-.75H9a.75.75 0 01.75.75v4.5a.75.75 0 01-1.5 0v-2.69l-3.97 3.97a.75.75 0 01-1.06-1.06l3.97-3.97H4.5a.75.75 0 01-.75-.75zm10.5 0a.75.75 0 01.75-.75h4.5a.75.75 0 010 1.5h-2.69l3.97 3.97a.75.75 0 11-1.06 1.06l-3.97-3.97v2.69a.75.75 0 01-1.5 0V15z" clip-rule="evenodd" />
                   </svg>
